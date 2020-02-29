506e1ce0550c3071019bd32b2f5f48e0f79c0f04
Jonathan Thaw <hi@jthaw.me>
2020-02-29 22:15:12 +0000

    added links section and started general design

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,118 @@
 <script>
-  import TransformExplorer from "./TransformExplorer.svelte";
-  import FilterExplorer from "./FilterExplorer.svelte";
+  import Explorer from "./components/Explorer.svelte";
+  import LinksList from "./components/LinksList.svelte";
+
+  const servicesLinks = [
+    { to: "https://www.figma.com/", label: "Figma", sublabel: "Design" },
+    {
+      to: "https://codesandbox.io/",
+      label: "Code Sandbox",
+      sublabel: "Development"
+    },
+    {
+      to: "https://code.visualstudio.com/",
+      label: "Visual Studio Code",
+      sublabel: "Development"
+    },
+    {
+      to: "https://www.netlify.com/",
+      label: "Netlify",
+      sublabel: "Hosting"
+    },
+    {
+      to: "https://www.netlify.com/",
+      label: "Netlify",
+      sublabel: "Hosting"
+    },
+    {
+      to: "https://pages.github.com/",
+      label: "Github Pages",
+      sublabel: "Hosting"
+    },
+    {
+      to: "https://glitch.com/",
+      label: "Glitch",
+      sublabel: "Code"
+    }
+  ];
+
+  const usefulLinks = [
+    { to: "https://www.jthaw.me/", label: "Slides" },
+    {
+      to: "https://jthaw.me",
+      label: "Starting Code",
+      sublabel: "Designing Layouts"
+    }
+  ];
+
+  const furtherLinks = [
+    {
+      to: "https://www.jthaw.me/",
+      label: "CSS Tricks",
+      sublabel: "Everything web"
+    },
+    {
+      to: "https://jthaw.me",
+      label: "MDN",
+      sublabel: "Web Documentation"
+    },
+    {
+      to: "https://jthaw.me",
+      label: "FFConf",
+      sublabel: "Creative Web Conference"
+    },
+    {
+      to: "https://jthaw.me",
+      label: "Codeacademy",
+      sublabel: "Online Course"
+    },
+    {
+      to: "https://jthaw.me",
+      label: "Lab.arts",
+      sublabel: "Creative Lab"
+    }
+  ];
 </script>
 
 <style lang="scss">
-  section {
-    scroll-snap-type: y mandatory;
+  .top {
+    padding: var(--size-unit-2) var(--size-unit-2);
+  }
+
+  .links-group {
+    display: grid;
+
+    grid-template-columns: repeat(12, 1fr);
+    grid-column-gap: var(--size-unit-2);
+
+    padding-top: var(--size-unit-2);
+
+    div {
+      grid-column-end: span 3;
+
+      &:last-of-type {
+        grid-column-start: 10;
+      }
+    }
   }
 </style>
 
 <main>
-  <section>
-    <TransformExplorer />
-    <FilterExplorer />
+  <section class="top">
+    <h1>UXD – Modern CSS</h1>
+
+    <div class="links-group">
+      <div>
+        <LinksList title="Services" links={servicesLinks} />
+      </div>
+      <div>
+        <LinksList title="Useful Links" links={usefulLinks} />
+      </div>
+
+      <div>
+        <LinksList title="Further Learning" links={furtherLinks} />
+      </div>
+    </div>
   </section>
+  <Explorer />
 </main>
---
+++ src/components/LinksList.svelte
@@ -0,0 +1,59 @@
+<script>
+  export let title = "";
+  export let links = [];
+</script>
+
+<style lang="scss">
+  .title {
+    font-size: var(--body-font-size);
+
+    margin: 0 0 var(--size-unit-4);
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+
+    list-style: none;
+
+    li {
+      margin-bottom: var(--size-unit-2);
+    }
+
+    a {
+      color: var(--color-text);
+      text-decoration: none;
+
+      span {
+        display: block;
+
+        color: var(--color-dark-grey);
+      }
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    ul {
+      a {
+        span {
+          display: inline;
+        }
+      }
+    }
+  }
+</style>
+
+<p class="title">{title}</p>
+
+<ul>
+  {#each links as link}
+    <li>
+      <a href={link.to} target="_blank">
+        {link.label}
+        {#if link.sublabel}
+          <span>– {link.sublabel}</span>
+        {/if}
+      </a>
+    </li>
+  {/each}
+</ul>
